b84a81b2383a6d0e09352287186e137659f3abda
jonas.heschl@gmx.at <jonas.heschl@gmx.at>
2020-02-24 19:14:43 +0100

    implement correct generation of TimedTeacherLocation instances

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import math
 from datetime import date
+from datetime import timedelta
 from bs4 import BeautifulSoup
 
-import TimedTeacherLocation
+from TimedTeacherLocation import TimedTeacherLocation
 from WebUntisParserUtil import *
 
-
-timedTeacherLocations = [TimedTeacherLocation]
+timedTeacherLocations: [TimedTeacherLocation] = []
 
 mondayte: date = date(2020, 2, 24)  # date of monday TODO: generate automatically
 with open('resources/WebUntis-3a-20200224.html') as fp:
@@ -16,13 +17,25 @@ with open('resources/WebUntis-3a-20200224.html') as fp:
     for idx_row, row in enumerate(rows):
         if row == '\n':
             continue
+        day_of_week: float = 0
         for idx_lesson, lesson in enumerate(row):
             if idx_lesson == 0:
                 continue
-            day_of_week: float = 0
             teachers: [str] = extract_teachers(lesson)
             location: str = extract_location(lesson)
             for teacher in teachers:
                 timedTeacherLocations.append(
-                    TimedTeacherLocation(teacher=teacher, location=location, lesson=idx_lesson, date=mondayte + day_of_week)
+                    TimedTeacherLocation(
+                        teacher=teacher,
+                        location=location,
+                        lesson=math.ceil(idx_row / 2),
+                        dt=mondayte + timedelta(days=int(day_of_week))
+                    )
                 )
+            if 'bl' in lesson.attrs['class']:
+                day_of_week += 0.5
+            if 'br' in lesson.attrs['class']:
+                day_of_week += 0.5
+
+for timedTeacherLocation in timedTeacherLocations:
+    print(timedTeacherLocation)
