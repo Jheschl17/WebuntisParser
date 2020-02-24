d2ea66cfc6ef961ae38ab6910ae429387581da3a
jonas.heschl@gmx.at <jonas.heschl@gmx.at>
2020-02-24 18:36:02 +0100

    minor refactor

--- TimedTeacherLocation.py
+++ TimedTeacherLocation.py
@@ -3,16 +3,16 @@ from datetime import date
 
 class TimedTeacherLocation:
 
-    def __init__(self, teacher: str, location: str, date: date, lesson: int):
-        self.teacher = teacher
-        self.location = location
-        self.date = date
-        self.lesson = lesson
-
     teacher: str
     location: str
     date: date
     lesson: int
+
+    def __init__(self, teacher: str, location: str, dt: date, lesson: int):
+        self.teacher = teacher
+        self.location = location
+        self.date = dt
+        self.lesson = lesson
 
     def __str__(self):
         return f'{{{self.teacher}, {self.location}, {self.date}, {self.lesson}}}'
