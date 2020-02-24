e8299a405baa984f6548c948e932573d7a102371
jonas.heschl@gmx.at <jonas.heschl@gmx.at>
2020-02-24 01:03:11 +0100

    add testing code

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+from bs4 import BeautifulSoup
+
+with open('resources/WebUntis-3a-20200224.html') as fp:
+    soup = BeautifulSoup(fp, 'html.parser')
+    table = soup.find(id='timetable').table.tbody
+    print(table)
