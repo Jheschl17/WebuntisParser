52fcc6bae1e226030a44fced7958a35ff889d4b4
jonas.heschl@gmx.at <jonas.heschl@gmx.at>
2020-02-24 22:28:20 +0100

    move html parsing to WebUntisParserUtil

--- main.py
+++ main.py
@@ -14,44 +14,14 @@ BeautifulSoup4 (https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
 __author__ = "StrauXX"
 
 
-import math
-from datetime import date
-from datetime import timedelta
-from bs4 import BeautifulSoup
-
-from TimedTeacherLocation import TimedTeacherLocation
 from WebUntisParserUtil import *
 
-timedTeacherLocations: [TimedTeacherLocation] = []
+
+timed_teacher_locations: [TimedTeacherLocation] = []
 
 mondayte: date = date(2020, 2, 24)  # date of monday TODO: generate automatically
-with open('resources/WebUntis-3a-20200224.html') as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
-    table = soup.find(id='timetable').table.tbody
-    rows = table.children
-    next(rows)
-    for idx_row, row in enumerate(rows):
-        if row == '\n':
-            continue
-        day_of_week: float = 0
-        for idx_lesson, lesson in enumerate(row):
-            if idx_lesson == 0:
-                continue
-            teachers: [str] = extract_teachers(lesson)
-            location: str = extract_location(lesson)
-            for teacher in teachers:
-                timedTeacherLocations.append(
-                    TimedTeacherLocation(
-                        teacher=teacher,
-                        location=location,
-                        lesson=math.ceil(idx_row / 2),
-                        dt=mondayte + timedelta(days=int(day_of_week))
-                    )
-                )
-            if 'bl' in lesson.attrs['class']:
-                day_of_week += 0.5
-            if 'br' in lesson.attrs['class']:
-                day_of_week += 0.5
+for soup in download_soups(mondayte):
+    timed_teacher_locations.extend(extract_timed_teacher_locations(soup, mondayte))
 
-for timedTeacherLocation in timedTeacherLocations:
-    print(timedTeacherLocation)
+for ttl in timed_teacher_locations:
+    print(ttl)
