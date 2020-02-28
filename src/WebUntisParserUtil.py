54e785d9a8e7a48b7a3ee51b32d91f250ca70795
jonas <jonas.heschl@gmx.at>
2020-02-28 20:43:41 +0100

    pc laptop sync

--- src/WebUntisParserUtil.py
+++ src/WebUntisParserUtil.py
@@ -1,5 +1,4 @@
 import math
-
 from bs4 import BeautifulSoup
 from bs4 import Tag
 from datetime import date, timedelta
@@ -30,34 +29,44 @@ def extract_location(lesson_outer: Tag) -> str:
 
 def extract_timed_teacher_locations(soup: BeautifulSoup, mondayte: date) -> [TimedTeacherLocation]:
     timed_teacher_locations: [TimedTeacherLocation] = []
-    date_offsets: [int] = []
     table = soup.find(id='timetable').table
+    date_offsets: [[bool]] = [[False] * 10] * calc_total_table_width(table)  # [day][lesson]
     rows = table.children
     next(rows)
     for idx_row, row in enumerate(rows):
         if row == '\n':
             continue
+        day_of_week: int = 0
+        filtered_row = row.children
+        filtered_row = filter(lambda it: 'ttcell' in lesson.attrs, filtered_row)
         for idx_lesson, lesson in enumerate(row):
             if idx_lesson == 0:
                 continue
-            if 'ttcell' in lesson.attrs['class']:
-                teachers: [str] = extract_teachers(lesson)
-                location: str = extract_location(lesson)
-                rowspan: int = int(lesson.attrs['rowspan']) if 'rowspan' in lesson.attrs else 1
-                for teacher in teachers:
-                    for i in range(rowspan):
-                        #  TODO add time to date_offset when rowspan > 1
-                        timed_teacher_locations.append(
-                            TimedTeacherLocation(
-                                teacher=teacher,
-                                location=location,
-                                lesson=math.ceil(idx_row / 2) + i,
-                                dt=mondayte + timedelta(days=date_offsets[idx_row])
-                            )
+            teachers: [str] = extract_teachers(lesson)
+            location: str = extract_location(lesson)
+            rowspan: int = int(lesson.attrs['rowspan']) if 'rowspan' in lesson.attrs else 1
+            for teacher in teachers:
+                for i in range(rowspan):
+                    timed_teacher_locations.append(
+                        TimedTeacherLocation(
+                            teacher=teacher,
+                            location=location,
+                            lesson=math.ceil(idx_row / 2) + i,
+                            dt=mondayte + timedelta(days=date_offsets[int(idx_row / 2) + 1])
                         )
-            if 'br' in lesson.attrs['class']:
-                date_offsets[idx_row] += 1
+                    )
+                    if 'br' in lesson.attrs['class']:
+                        day_of_week += 1
+                        date_offsets[int(idx_row / 2) + i] += 1
     return timed_teacher_locations
+
+
+def calc_total_table_width(table: Tag) -> int:
+    width_counter: int = 0
+    for lesson in list(list(table.children)[2].children)[1:]:
+        lesson_duration: int = int(lesson.attrs['colspan']) if 'colspan' in lesson.attrs else 1
+        width_counter += lesson_duration
+    return width_counter
 
 
 def download_soups(mondayte: date) -> [BeautifulSoup]:
